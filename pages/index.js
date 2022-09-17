5f10a5fb8257b5c373f5323a54d21964eddf9ffc
Elijah-Hensel <elijah.j.hensel@gmail.com>
2022-09-17 00:34:07 -0500

    added next/head to index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Head from 'next/head'
 import types from 'prop-types'
 import styles from '../styles/Home.module.css'
 import { getUsers } from './api'
@@ -19,13 +20,19 @@ const Home = ({ initialUsers }) => {
   const [users, setUsers] = useState(initialUsers)
 
   return (
-    <div className={styles.main}>
-      <SetupToastContainer />
-      <div className={styles.container}>
-        <span>Sign up to receive a The Beatles lyric every minute!</span>
-        <Form users={users} setUsers={setUsers} />
+    <>
+      <Head>
+        <title>Cope Notes: Elijah Hensel</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className={styles.main}>
+        <SetupToastContainer />
+        <div className={styles.container}>
+          <span>Sign up to receive a The Beatles lyric every minute!</span>
+          <Form users={users} setUsers={setUsers} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
